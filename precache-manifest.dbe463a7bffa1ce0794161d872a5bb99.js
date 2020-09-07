8843de42654def15ce1c7cc13301705e4dd39bf7
jundic <jundi_chen@sfu.ca>
2020-09-07 08:20:51 -0700

    Updates

---
+++ precache-manifest.dbe463a7bffa1ce0794161d872a5bb99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7fecaafe45f84184629865face5e62",
+    "url": "/thirteenFrontLoginRegister/index.html"
+  },
+  {
+    "revision": "5f0c6fd023bc778c7e2f",
+    "url": "/thirteenFrontLoginRegister/static/css/main.49290985.chunk.css"
+  },
+  {
+    "revision": "2bd639ae3390b3023cea",
+    "url": "/thirteenFrontLoginRegister/static/js/2.d9ecf7f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thirteenFrontLoginRegister/static/js/2.d9ecf7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0c6fd023bc778c7e2f",
+    "url": "/thirteenFrontLoginRegister/static/js/main.0ff444b6.chunk.js"
+  },
+  {
+    "revision": "67324a7b86d71ced5989",
+    "url": "/thirteenFrontLoginRegister/static/js/runtime-main.5ab382cf.js"
+  },
+  {
+    "revision": "bb3dc6efd5d408579f92d3dc4355e034",
+    "url": "/thirteenFrontLoginRegister/static/media/eddieLogo.bb3dc6ef.png"
+  }
+]);
